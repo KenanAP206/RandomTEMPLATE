6b8920230332dfaa6eb872eeb22625bc4436da92
nazriin <ibabazade014@gmail.com>
2025-06-25 16:49:04 +0400

    commit

--- vite-project/src/Components/User/Navbar/index.jsx
+++ vite-project/src/Components/User/Navbar/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './index.css'
 
 function index() {
   return (
-    <div>
-      Navbar
-    </div>
+    <>
+    <section id="navbar">
+      <div className="left">
+       <Link>Start Bootstrap</Link>
+       <Link>Home</Link>
+       <Link> About</Link>
+       <Link> Shop</Link>
+       
+      </div>
+      <div className="right">
+        <button>CART</button>
+      </div>
+    </section>
+    <section id="banner">
+      <h1>SHOP IN STYLE</h1>
+    </section>
+    </>
   )
 }
 
 export default index
+
+
